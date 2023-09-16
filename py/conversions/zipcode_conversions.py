ef83f9422976fea40a34c010e28f830d57ee0e80
lukas <lschulte.neu@gmail.com>
2023-09-16 11:58:30 -0700

    Noodling

---
+++ py/conversions/zipcode_conversions.py
@@ -0,0 +1,17 @@
+
+##############
+# TO ZIPCODE #
+##############
+
+def ipv4ToGeo(ipv4):
+    print("TODO!")
+    return
+
+
+def phoneToZip(phone):
+    print("TODO!")
+    return
+
+def phoneToCountry(phone):
+    print("TODO!")
+    return
